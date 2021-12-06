dc4bfb272d306694e013b23e29a3512563d7f7a1
danielkarlsson2 <daniel@dkmusic.se>
2021-12-06 10:00:18 +0100

    lektion 1

--- Lektion-1/2-Console/script.js
+++ Lektion-1/2-Console/script.js
@@ -1,5 +1,28 @@
+//en kommentar på en rad // (commandK, commandC)
+/*  (shift, command A)
+    En kommentar kan skrivas så här 
+    om det är flera rader
+*/
+
+// Skriver ut någonting i konsolen
 console.log('Här skriver jag ut någonting i konsolen')
 console.warn('Detta är en varning')
 console.error('Detta är ett felmeddelande')
 
-console.log('hej')
+// console.log('hej')
+
+
+// Skriver ut på själva hemsidan
+document.body.innerText = 'Detta är en text'
+
+// Högnivåspråk   (Mellan användaren och systemet)
+// -----------------------------
+// (Javascript) 
+// Python
+// C# / Java (inget med javascript att göra)
+
+// Lågnivåspråk (Mellan systemet och hårdvara)
+// -------------------------------------------
+// C++
+// C
+// Mikrokod
