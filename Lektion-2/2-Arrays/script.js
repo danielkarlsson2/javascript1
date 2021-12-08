9f8633ece3c03014171f202bea69c7157a9883c4
danielkarlsson2 <daniel@dkmusic.se>
2021-12-08 13:59:37 +0100

    Lektion 2

--- Lektion-2/2-Arrays/script.js
+++ Lektion-2/2-Arrays/script.js
@@ -30,29 +30,104 @@ const names = ['Daniel', 'Alice', 'Ines', 'Hanna'];
 
 // Lägga till i början av en array
 names.unshift('Colin');
-console.log('Med unshift ' + names);
+// console.log('Med unshift ' + names);
 
 // Ta bort i början av en array
 names.shift();
-console.log('Med shift: ' + names);
+// console.log('Med shift: ' + names);
 
 // Lägga till i slutet av en array
 names.push('Colin');
-console.log('Med push: ' + names);
+// console.log('Med push: ' + names);
 
 // Ta bort i slutet av en array
 names.pop();
-console.log('Med pop: ' + names);
+// console.log('Med pop: ' + names);
 
 // Ändra ett specifik värde i en array
 names[1] = 'Nytt namn';
-console.log('Ändrat: ' + names);
+// console.log('Ändrat: ' + names);
 
 // names = ['Tommy', 'Annika']; //Går inte när det är en const
 
-console.log(names.indexOf('Nytt namn'));    //get -1 om det inte finns
+// Hämta ut vilken indexplats någonting har i en array.
+names.indexOf('Nytt namn');    //ger -1 om det inte finns
 
 // const i = names.indexOf('Nytt namn');
 // names[i] = 'hej';
 names[names.indexOf('Nytt namn')] = 'Hej!';
+// console.log(names);
+
+// Delete - tar bort värdet på en specifik plats i en array men inte platsen
+// delete names[1];
+// delete names[names.indexOf('Hej')];
+
+// console.log(names);
+
+// .splice()    -   Tar bort och / eller lägger till ett värde från en specifik position
+names.splice(1, 1); //Tar bort 1 från indexplats 1
+names.splice(1, 0, 'Colin', 'Alice');   //Tar bort 0 och lägger till Colin & Alice
+names.splice(1, 2, 'Jack');     //Tar bort 2 från indexplats 1 och lägger till Jack
+names.splice(names.indexOf('Jack'), 1, 'Tommy', 'Pippi');
+
+// console.log(names);
+
+// Hur lång är en array
+// console.log(names.length);
+
+// const arrayToAdd = ['Colin', 'Vince']
+
+// LÄgg ihop två arrayer
+// console.log(names.concat(['Colin', 'Vince']));  //Görs bara i consolen (modifierar inte det ursprungliga värdet)
+// const newNames = names.concat(['Colin', 'Vince'])    //Returnerar en ny kombinerad array
+
+// Spread operator
+
+// names.splice(names.length -1, 0, ...arrayToAdd);
+// names.splice(0, names.length, ...arrayToAdd);
+// console.log(names)
+
+
+
+const obj = {
+    fName: 'Daniel',
+    lName: 'Karlsson'
+}
+
+const obj2 = {
+    email: 'daniel@dkmusic.se',
+    obj: {
+        fName: 'Daniel',
+        lName: 'Karlsson'
+    }
+}
+
+const obj3 = {
+    ...obj,
+    email: 'daniel@dkmusic.se'
+}
+
+// const arr = [...gamla, nåtnytt]
+
+// console.log(obj2);
+// console.log(obj3);
+
+// console.log(names);
+// names.sort();   //Sortera i bokstasordning
+
+const newNames = ['Aa', 'Bb', '1a', '2a', 'aa', 'bb']
+const numbers = [5, 6, 7, 8, 9, 10, 11, 12, 30, 40];
+
+
 console.log(names);
+// names.reverse(); //Vänder på arrayen så först blir sist osv.
+// console.log(names);
+
+// names.sort().reverse(); // Sortera i bokstavsordning baklänges
+// console.log(names)
+
+newNames.sort();
+console.log(newNames);
+
+numbers.sort();
+console.log(numbers);
