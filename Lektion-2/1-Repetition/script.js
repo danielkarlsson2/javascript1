1e59a952d4b417c56cccd2ed276d36b2f8266d3e
danielkarlsson2 <daniel@dkmusic.se>
2021-12-08 09:38:57 +0100

    Lektion 2

---
+++ Lektion-2/1-Repetition/script.js
@@ -0,0 +1,43 @@
+// CONSOLE
+//Vanligast att felsöka med konsolen. 
+//Felsöknings om någonting går fel
+// let app;
+// let app;
+
+// (Kommer använda konsolloggen hela tiden.)
+
+// console.log('vanligt meddelande');
+// console.warn('varningsmeddelande');
+// console.error('felmeddelande');
+
+// DOM
+// document.body.innerText = '<h1>Hejsan</h1>';
+// document.body.innerHTML = '<h1>Hejsan</h1>';
+// document.body.createElement('div').innerText = 'hej';    //ger ett felmeddelande
+// document.body.appendChild(document.createElement('div'));
+
+// const results = document.querySelector('#results');
+// const input = document.querySelector('.input-class');
+// const input = document.getElementsByName('input-name')[0];
+// const input = document.getElementsById('input-id');
+
+// let res = document.querySelector('#input-id').value;
+
+// res = input.value;
+// results.innerText = res;
+
+
+// console.log(input);
+// console.log(res)
+
+
+// VARIABLES
+
+// name = 'Daniel';        //Deklarerar någonting globalt. GÖR INTE SÅ
+// var name = 'Daniel';    //Deklarerar någonting globalt. UNDVIKA DET HÄR
+// let name = 'Daniel'     // Deklarerar någonting lokalt i ett scope. GÖR DET HÄR
+// const name = 'Daniel'      // Deklarerar någonting skrivskyddat lokalt. GÖR DET HÄR
+
+// name = 'DanyK'
+
+// console.log(name);
---
+++ Practise/W3-practise/script.js
@@ -0,0 +1,8 @@
+// onclick=document.getElementById("test").innerHTML'= "Hello JavaScript";
+// document.getElementById("testar").innerHTML="Första javascriptet";
+function myFunction() {
+    document.getElementById("test").innerHTML = "Nu har texten ändrats";
+}
+
+let a, b, c;
+a = 1; b=2; c=a+b
