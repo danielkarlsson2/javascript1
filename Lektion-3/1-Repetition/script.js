195ca5ecb53a01d122e749f4818b0fc08912cbaa
danielkarlsson2 <daniel@dkmusic.se>
2021-12-13 10:55:30 +0100

    Lektion 3

--- Lektion-3/1-Repetition/script.js
+++ Lektion-3/1-Repetition/script.js
@@ -63,26 +63,86 @@ const family = ['Kjell', 'Gun-britt', 'Andreas', 'Daniel', 'Hanna', 'Ines'];
 
 family.sort();
 
-console.log(family);
+// console.log(family);
 
-console.log(family.indexOf('Daniel'));
+// console.log(family.indexOf('Daniel'));
 
 family.splice(family.indexOf('Daniel')+1, 0, 'Hans', 'Tommy');
 
-console.log(family);
+// console.log(family);
 
 family.pop();
 family.unshift('Kjell');
 
-console.log(family);
+// console.log(family);
 
 
 
 let counter = 0;
 family.forEach(() => {
     counter++;
-    console.log(counter);
+    // console.log(counter);
 })
 
-console.log(family.length);
+// console.log(family.length);
 
+const greetings = family.map(name => {
+    return 'Hejsan ' + name;
+})
+
+greetings.forEach(greet => {
+    // console.log(greet);
+})
+
+// --------------END------------------
+
+
+// let selected = array.map(person=> {
+//     return person.lastName == 'Karlsson';
+// })
+
+// FILTER tar bort det som inte matchar
+// let selected = array.filter(person => {
+//     return person.lastName == 'Karlsson';
+// })
+
+// Gör man bara en enda sak kan man skriva på samma rad, då blir => samma som return
+// let selected = array.filter(person => {
+//     return person.lastName == 'Karlsson';
+// })
+// .map(person => {
+//     return person.firstName
+// })
+// .sort();
+
+// console.log(selected);
+
+// const shoppingList = ['mjölk', 'ägg', 'bröd'];
+
+// console.log(shoppingList.includes('smör'));
+
+// SET
+
+const shoppingList = new Set();
+shoppingList.add('mjölk');
+shoppingList.add('ägg');
+shoppingList.add('smör');
+shoppingList.add('ägg');
+shoppingList.add('ägg');
+shoppingList.add('ägg');
+shoppingList.add('bröd');
+
+
+console.log(shoppingList);
+
+// const list = [];
+shoppingList.forEach(item => {
+    // console.log(item);
+    // list.push(item);
+})
+
+// let list = Array.from(shoppingList);
+// const list = [...shoppingList];
+const list = [...shoppingList];
+
+console.log(list)
---
+++ Lektion-3/2-Loops/scripts.js
@@ -0,0 +1,64 @@
+// LOOPAR
+
+const namesArray = ['Joakim', 'Jeanette', 'Hans', 'Tommy', 'Jack'];
+// const usersArray = [
+//     {name: 'Daniel', email: 'daniel@dkmusic.se', age: 35, status: true}
+//     {name: 'Joakim', email: 'joakim@gmail.com', age: 34, status: false}
+//     {name: 'Hans', email: 'hans@gmail.com', age: 36, status: false}
+//     {name: 'Tommy', email: 'daniel@tommy@gmail.com', age: 38, status: true}
+// ]
+
+// FOR Loopar - Loopar ett givet antal gånger
+
+// for (let i = 0 ; i < 5; i++) {  
+//     console.log(i);
+// }
+// for (let i = 0 ; i < 5; i++) {  
+//     console.log(i);
+//     console.log(namesArray[i])
+// }
+
+// // Loopar igenom hela en array
+// for (let i = 0; i < namesArray.length; i++) {
+//     console.log(i + ' - ' + namesArray[i]);
+// }
+
+// -----------------------------------
+
+// FOR OF Loopar - Loopar igenom en hel array
+
+// for(let name of namesArray) {
+//     console.log(name)
+// }
+
+// for (let user of usersArray) {
+//     console.log(user)
+// }
+
+// --------------
+// WHILE loopar
+// Kom ihåg att inkrementera i, annars blird et en oändlig loop
+
+// let i = 1;
+
+// console.log(i < 5);
+
+// while( i < 5 ) {
+//     console.log('while: '+ i);
+//     i++;
+// }
+
+// console.log(i < 5);
+// console.log('i är: ' + i);
+
+// // DO WHILE - Kommer alltid att köras minst en gång
+
+// do {
+//     console.log('do while: ' + i)
+//     i++;
+// } while (i < 7);
+
+// console.log('i är ' +i);
+
+
+// --------------------------------------------------------
